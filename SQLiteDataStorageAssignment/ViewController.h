fcb554653a35bca215d407cfa655e064502db891
Apurva Kumari <apurvakumari@Apurvas-MacBook-Pro.local>
2017-10-04 16:46:50 -0700

    Adding SQLite Data Storage code

--- SQLiteDataStorageAssignment/ViewController.h
+++ SQLiteDataStorageAssignment/ViewController.h
@@ -7,8 +7,32 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <sqlite3.h>
 
 @interface ViewController : UIViewController
+
+@property (strong, nonatomic) NSString *databasePath;
+@property (nonatomic) sqlite3 *myContactDB;
+
+@property (weak, nonatomic)IBOutlet UILabel *SQLitestorage;
+@property (weak, nonatomic)IBOutlet UILabel *contactnameLabel;
+@property (weak, nonatomic)IBOutlet UILabel *locationLabel;
+@property (weak, nonatomic)IBOutlet UILabel *addressLabel;
+@property (weak, nonatomic)IBOutlet UILabel *phoneNumberLabel;
+@property (weak, nonatomic)IBOutlet UILabel *emailLabel;
+@property (weak, nonatomic) IBOutlet UILabel *status;
+
+@property (weak, nonatomic) IBOutlet UITextField *contactname;
+@property (weak, nonatomic) IBOutlet UITextField *location;
+@property (weak, nonatomic) IBOutlet UITextField *address;
+@property (weak, nonatomic) IBOutlet UITextField *phoneNumber;
+@property (weak, nonatomic) IBOutlet UITextField *email;
+
+@property (weak, nonatomic) IBOutlet UIButton *saveButton;
+@property (weak, nonatomic) IBOutlet UIButton *cancelButton;
+
+- (IBAction)saveContact:(id)sender;
+- (IBAction)cancel:(id)sender;
 
 
 @end
--- SQLiteDataStorageAssignment/ViewController.m
+++ SQLiteDataStorageAssignment/ViewController.m
@@ -16,7 +16,46 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *docsDir;
+    NSArray *dirPaths;
+    
+    // Get the documents directory
+    dirPaths = NSSearchPathForDirectoriesInDomains(
+      NSDocumentDirectory, NSUserDomainMask, YES);
+    
+    docsDir = dirPaths[0];
+    
+    // Build the path to the database file
+    _databasePath = [[NSString alloc]
+                     initWithString: [docsDir stringByAppendingPathComponent:
+                                      @"myContacts.db"]];
+    
+    
+    NSLog(@"\nDBPath %@\n", docsDir);
+    
+    
+    NSFileManager *filemgr = [NSFileManager defaultManager];
+    
+    if ([filemgr fileExistsAtPath: _databasePath ] == NO)
+    {
+        const char *dbpath = [_databasePath UTF8String];
+        
+        if (sqlite3_open(dbpath, &_myContactDB) == SQLITE_OK)
+        {
+            char *errMsg;
+            const char *sql_stmt =
+            "CREATE TABLE IF NOT EXISTS CONTACTS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, LOCATION TEXT , ADDRESS TEXT, PHONE TEXT, EMAIL TEXT)";
+            
+            if (sqlite3_exec(_myContactDB, sql_stmt, NULL, NULL, &errMsg) != SQLITE_OK)
+            {
+                _status.text = @"Failed to create table";
+            }
+            sqlite3_close(_myContactDB);
+        } else {
+            _status.text = @"Failed to open/create database";
+        }
+    }
 }
 
 
@@ -24,6 +63,45 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)saveContact:(id)sender {
+    sqlite3_stmt    *statement;
+    const char *dbpath = [_databasePath UTF8String];
+    
+    if (sqlite3_open(dbpath, &_myContactDB) == SQLITE_OK)
+    {
+        
+        NSString *insertSQL = [NSString stringWithFormat:
+                               @"INSERT INTO CONTACTS (name, location, address, phone, email) VALUES (\"%@\", \"%@\", \"%@\", \"%@\", \"%@\")",
+                               _contactname.text, _location.text, _address.text, _phoneNumber.text, _email.text];
+        
+        const char *insert_stmt = [insertSQL UTF8String];
+        sqlite3_prepare_v2(_myContactDB, insert_stmt,
+                           -1, &statement, NULL);
+        if (sqlite3_step(statement) == SQLITE_DONE)
+        {
+            _status.text = @"Contact added";
+            _contactname.text = @"";
+            _location.text = @"";
+            _address.text = @"";
+            _phoneNumber.text = @"";
+            _email.text = @"";
+        } else {
+            _status.text = @"Failed to add contact";
+        }
+        sqlite3_finalize(statement);
+        sqlite3_close(_myContactDB);
+    }
+    
+}
 
+-(IBAction)cancel:(id)sender {
+    _contactname.text = @"";
+    _location.text = @"";
+    _address.text = @"";
+    _phoneNumber.text = @"";
+    _email.text = @"";
+    _status.text = @"";
+    
+}
 
 @end
